64873980d94d6f1b6bd053a2800d859ff8979274
Mohit Ramchandani <Mohitr35@gmail.com>
2013-11-20 15:52:18 -0800

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brickbat::Application.config.secret_token = 'de2935ac221dc4dcc3ea136fa05367d497a5077b01e777b6e29300043fef85f642386bc930df9ca475bff8be013b314d59a8c47e8168e23f6a187bd2b915a391'
