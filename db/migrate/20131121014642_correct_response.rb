876eab7b8b507dc5060c3f5a7fb0b7a29ca02319
MarionKnight <ryanhedges@gmail.com>
2013-11-21 14:55:00 -0800

    Removed correct_response migration.

--- db/migrate/20131121014642_correct_response.rb
+++
@@ -1,7 +0,0 @@
-class CorrectResponse < ActiveRecord::Migration
-  def change
-    add_column :responses, :post_id, :integer
-    remove_column :responses, :response_id
-    change_column :responses, :votes, :integer, default: 0
-  end
-end
