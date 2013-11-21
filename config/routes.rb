e214de639db116a252a2d361723264e7afa31fb2
TGOlson <tydotg@gmail.com>
2013-11-20 18:10:53 -0800

    Modify routes to only include new and create for responses, exclude edit update and destroy for posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Brickbat::Application.routes.draw do
 
-  resources :posts
-  resources :responses
+  resources :posts, except: [:edit, :update, :destroy]
+  resources :responses, only: [:new, :create]
 
 end
