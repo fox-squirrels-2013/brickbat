ef2593be91ded02ef526f6d0f14a48012ee6ec4d
trostli <ryanhedges@gmail.com>
2013-11-22 12:04:50 -0800

    Controller tests for creating new / updating votes.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,19 @@
 class VotesController < ApplicationController
 
   def create
-    vote = Vote.new params[:vote]
+
+    vote = Vote.where(user_id: session[:user_id], 
+                      response_id: params[:vote][:response_id]).first
+    if vote.nil?
+      vote = Vote.new user_id: session[:user_id],
+                      response_id: params[:vote][:response_id],
+                      vote: params[:vote][:vote]
+    else
+      vote.vote = params[:vote][:vote]
+    end
+
     if vote.save
-      render json: { response: 'Up vote registered'}
+      render json: { response: "Vote registered", vote: vote.vote}
     else
       render json: { error: 'There was an error saving the vote.'}
     end
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe VotesController do
 
-  let(:vote_params){ { user_id: 1, response_id: 1, vote: 'Up' } }
+  let!(:set_user) { session[:user_id] = 1}
+  let(:vote_params){ { response_id: 1, vote: 'Up' } }
 
-  context "Can create new votes" do
-
+  context "creates new votes where no user/response exists" do
     it "should create an up vote with valid params" do
       expect{
         post :create, vote: vote_params
-        expect(parse_response_body('response')).to eq "Up vote registered"
+        expect(parse_response_body('response')).to eq "Vote registered"
+        expect(parse_response_body('vote')).to eq "Up"
         }.to change(Vote, :count).by 1
     end    
 
@@ -19,8 +20,23 @@ describe VotesController do
         expect(parse_response_body('error')).to_not be_nil
         }.to change(Vote, :count).by 0
     end
+  end
 
-    
+  context "does not create new votes where a user/response already exists" do
+    it "should not create a new vote object" do
+        Vote.create! user_id: 1, response_id: 1, vote: 'Down'
+        # try to create vote again with different vote action
+      expect{
+        post :create, vote: vote_params
+        }.to change(Vote, :count).by 0
+    end
+
+    it 'should update existing vote with new vote type' do
+        Vote.create! user_id: 1, response_id: 1, vote: 'Down'
+        post :create, vote: vote_params
+        expect(parse_response_body('vote')).to eq "Up"
+        expect(Vote.last.vote).to eq 'Up'
+    end
   end
 
 end
