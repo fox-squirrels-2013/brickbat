2635fdaed949a7acee2eb0e43fbc8199f45f056b
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-21 16:06:47 -0800

    Added Capybara::DSL to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
-include Capybara::DSL
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -40,6 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  config.include Capybara::DSL
 end
 
 OmniAuth.config.test_mode = true
