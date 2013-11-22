4345e186791706291d8e79f20efa2d152ad82f4d
trostli <ryanhedges@gmail.com>
2013-11-22 11:33:28 -0800

    controller tests for creating new vote

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,19 +2,22 @@ require 'spec_helper'
 
 describe VotesController do
 
-
-
-    #   t.integer "user_id"
-    # t.integer "response_id"
-    # t.string  "vote"
-
   let(:vote_params){ { user_id: 1, response_id: 1, vote: 'Up' } }
 
   context "Can create new votes" do
-    it "should create an up vote from a user, for a response" do
+
+    it "should create an up vote with valid params" do
       expect{
         post :create, vote: vote_params
+        expect(parse_response_body('response')).to eq "Up vote registered"
         }.to change(Vote, :count).by 1
+    end    
+
+    it "should not create a vote with invalid params" do
+      expect{
+        post :create, vote: {}
+        expect(parse_response_body('error')).to_not be_nil
+        }.to change(Vote, :count).by 0
     end
 
     
