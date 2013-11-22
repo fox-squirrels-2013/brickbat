c2f4bd049663db288e0c387bfa39e14d9992aa58
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-21 21:23:37 -0800

    Added test environment details for user testing.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,22 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
 end
+
+OmniAuth.config.test_mode = true
+
+info_hash = OmniAuth::AuthHash::InfoHash.new
+
+info_hash.nickname = "testuser"
+
+auth_credentials = OmniAuth::AuthHash::InfoHash.new
+
+auth_credentials.token = "123123123123"
+auth_credentials.secret = "321123321123"
+
+auth_hash = OmniAuth::AuthHash.new
+auth_hash.provider = 'twitter'
+auth_hash.uid = 123456
+auth_hash.credentials = auth_credentials
+auth_hash.info = info_hash
+
+OmniAuth.config.mock_auth[:twitter] = auth_hash
