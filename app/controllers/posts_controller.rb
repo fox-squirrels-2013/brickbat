64f66d581b949267f02f80814184455c811fd660
tgolson <tydotg@gmail.com>
2013-11-22 18:32:57 -0800

    sass styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,14 +15,14 @@ class PostsController < ApplicationController
 
   def create
     user = User.find_by_id(session[:user_id])
-    # client = TwitterHelper.new(user)
+    client = TwitterHelper.new(user)
 
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
     @post.user_id = session[:user_id]
     if @post.save
-      # client.update(@post.body)
+      client.update(@post.body)
       redirect_to post_path(@post)
     else
       session[:errors] = @post.errors
